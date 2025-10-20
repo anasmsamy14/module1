5ee76513434018b6368af63c5b3bbe4c54ab49ef
anasmsamy14 <anas.m.samy14@gmail.com>
2025-10-20 17:42:02 +0300

    message

---
+++ lesson 3.py
@@ -0,0 +1,12 @@
+name="Anas"
+print(name)
+print("data type of name",type(name))
+age=11
+print(age)
+print("data type of age",type(age))
+num=0.187
+print(num)
+print("data type of num",type(num))
+me=False
+print(me)
+print("data type of num",type(me))
