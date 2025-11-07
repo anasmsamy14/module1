3c52600d2ae61ab7e3bdc7cc97a8c30d9ed0301d
anasmsamy14 <anas.m.samy14@gmail.com>
2025-11-07 14:59:15 +0200

    !=

--- not equal to.py
+++ not equal to.py
@@ -1,7 +1,13 @@
-a= int(input("Enter a number"))
-b= int(input("Enter another number"))
+a= int(input("Enter a number = "))
+b= int(input("Enter another number= "))
 if(a!=b):
     print("they are not equal ")
 else:
     print("they are equal")
  
+a= str(input("enter a string= "))
+b=str(input("enter another string= "))
+if(a!=b):
+    print('they are not equal to')
+else:
+    print('they are equal')
