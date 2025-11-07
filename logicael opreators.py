68f5e3e378c9c435c94fbf17339c53faa859f5d2
anasmsamy14 <anas.m.samy14@gmail.com>
2025-11-07 14:39:24 +0200

    logical operators

---
+++ logicael opreators.py
@@ -0,0 +1,20 @@
+a=30
+b=30
+c=0
+print("a=",a, "\nb=",b, "\nc=",c)
+if a and b and c:
+    print("all numbers have boolean value as true")
+else:
+    print("atleast one number has boolean value as false")
+a=10
+b=-10
+c=0
+print("a=",a,"\nb=",b,"\nc=",c)
+if(a>0 or b>0):
+    print("Either a or b are greater than zero")
+else:
+    print("no number is greater than zero")
+if(b>0 or c>0):
+     print("Either b or c are greater than zero")
+else:
+    print("no number is greater than zero")
