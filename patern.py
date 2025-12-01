c2c1bb62ea3af926b1f37e1c1f70ba6caa3c1f0f
anasmsamy14 <anas.m.samy14@gmail.com>
2025-12-01 17:50:09 +0200

    hi

---
+++ patern.py
@@ -0,0 +1,7 @@
+ask=int(input("enter a number of rows you want to be in a right angled triangle: "))
+
+for i in range (0,ask):
+    for j in range (0,i+1):
+        print("*",end="")
+    print()
+    
