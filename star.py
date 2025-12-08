876dd1585de88e71fd408fa9242f32ae9c7a87fe
anasmsamy14 <anas.m.samy14@gmail.com>
2025-12-08 17:52:19 +0200

    in class

---
+++ star.py
@@ -0,0 +1,26 @@
+import turtle
+t=turtle.Turtle()
+
+t.forward(100)
+
+t.left(120)
+t.forward(100)
+
+t.left(120)
+t.forward(100)
+
+t.penup()
+t.right(150)
+t.forward(50)
+
+t.pendown()
+t.right(90)
+t.forward(100)
+
+t.right(120)
+t.forward(100)
+
+t.right(120)
+t.forward(100)
+
+turtle.done()
