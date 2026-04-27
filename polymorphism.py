81cbb5a3da967348850e1ea00503c67329e52fc7
anasmsamy14 <anas.m.samy14@gmail.com>
2026-04-27 17:50:13 +0300

    inc

---
+++ polymorphism.py
@@ -0,0 +1,30 @@
+class Egypt():
+    def capital(self):
+        print("Cairo is the capital of Egypt")
+    
+    def language(self):        
+        print("The official language of Egypt is Arabic")
+    
+    def population(self):
+        print("The population of Egypt is approximately 120 million people")
+
+class India():
+    def capital(self):
+        print("New Delhi is the capital of India")
+    
+    def language(self):        
+        print("The official language of India are Hindi ")
+    
+    def population(self):
+        print("The population of India is approximately 1.4 billion people")
+
+obj_egypt = Egypt()
+obj_egypt.capital()
+obj_egypt.language()
+obj_egypt.population()
+obj_india = India()
+obj_india.capital() 
+obj_india.language()
+obj_india.population()
+
+
