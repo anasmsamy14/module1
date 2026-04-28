29e53762dc376fb1ea4d7914a1ae2aa3edcb776d
anasmsamy14 <anas.m.samy14@gmail.com>
2026-04-28 22:46:41 +0300

    act

--- fare.py
+++ fare.py
@@ -6,11 +6,8 @@ class Vehicle:
 class Bus(Vehicle):
     def fare(self, capacity):
         total = super().fare(capacity)
-        return total + (total * 0.10)   # add 10%
-
-
-# Create bus object
+        return total + (total * 0.10)   
 bus = Bus()
 
-# Seating capacity = 50
+
 print("Total Bus Fare:", bus.fare(50))
