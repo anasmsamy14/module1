9880c14dcc3d309286e76fc417cacb6aa7b8731d
anasmsamy14 <anas.m.samy14@gmail.com>
2026-04-28 22:35:18 +0300

    act

---
+++ car.py
@@ -0,0 +1,54 @@
+class BMW:
+    def fuel_type(self):
+        print("BMW uses Petrol")
+
+    def max_speed(self):
+        print("BMW max speed is 250 km/h")
+
+    def where_made(self):
+        print("BMW is made in Germany")
+
+
+class Ferrari:
+    def fuel_type(self):
+        print("Ferrari uses Petrol")
+
+    def max_speed(self):
+        print("Ferrari max speed is 340 km/h")
+
+    def where_made(self):
+        print("Ferrari is made in Italy")
+
+
+class Tesla:
+    def fuel_type(self):
+        print("Tesla uses Electricity")
+
+    def max_speed(self):
+        print("Tesla max speed is 260 km/h")
+
+    def where_made(self):
+        print("Tesla is made in USA")
+
+
+# Create objects
+b = BMW()
+f = Ferrari()
+t = Tesla()
+
+# Call methods manually (no loop)
+b.fuel_type()
+b.max_speed()
+b.where_made()
+
+print()
+
+f.fuel_type()
+f.max_speed()
+f.where_made()
+
+print()
+
+t.fuel_type()
+t.max_speed()
+t.where_made()
